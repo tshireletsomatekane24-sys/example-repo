487460708ba27bb222d4d89f8d2f3b1eb248eef6
Tshireletso <tshireletsomatekane24@gmail.com>
2026-05-16 12:52:05 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+# inserting an input function that requires a user to type in user's name and users age and then print out a message that says "Hello_World"
+name = input("What is your name?: ")
+print(name)
+age = input("How old are you? ")
+print(age)
+print("hello_world")
+
+#print(f"Hello {name}, you are {age} years old.")
+ 
